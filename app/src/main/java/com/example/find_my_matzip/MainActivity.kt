c9e0b923b7adc35e59a69dcd45a949d8a6eb0719
jurwon <susorrow@naver.com>
2023-11-14 17:57:49 +0900

    first commit

---
+++ app/src/main/java/com/example/find_my_matzip/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.find_my_matzip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
