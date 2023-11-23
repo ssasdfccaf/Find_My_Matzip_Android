358a09267428d28be7c3608b0781d4de19e2536f
김경태 <kkt8274@gmail.com>
2023-11-23 15:30:01 +0900

    HomeFollowViewPagerAdapter

--- app/src/main/java/com/example/find_my_matzip/navTab/adapter/HomeFollowViewPagerAdapter.kt
+++ app/src/main/java/com/example/find_my_matzip/navTab/adapter/HomeFollowViewPagerAdapter.kt
@@ -17,8 +17,10 @@ class HomeFollowViewPagerAdapter  : RecyclerView.Adapter<HomeFollowViewPagerAdap
     }
 
     class ViewHolder(val binding: ViewPagerHomeColorBinding) : RecyclerView.ViewHolder(binding.root) {
+        //        fun bind(item: String) {
         fun bind(item: String) {
-            binding.viewPagerColor.text = item
+            //뷰페이저에 글은 필요없다.
+//            binding.viewPagerColor.text = item
             binding.pagerImg.setOnClickListener {
                 Toast.makeText(binding.root.context,"뷰페이저 터치됨", Toast.LENGTH_SHORT).show();
                 Log.d("kkt","뷰페이저 사진 선택됨")
