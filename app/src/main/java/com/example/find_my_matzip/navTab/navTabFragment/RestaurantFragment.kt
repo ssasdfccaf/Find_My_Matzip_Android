91d25bc6b5db4dbf3c16bcf040a37fd7f5c1fced
옥 <eastok95@gmail.com>
2023-11-27 09:06:38 +0900

    reswithscore 추가

--- app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/RestaurantFragment.kt
+++ app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/RestaurantFragment.kt
@@ -9,7 +9,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.find_my_matzip.MyApplication
 import com.example.find_my_matzip.databinding.FragmentRestaurantBinding
+import com.example.find_my_matzip.model.ResWithScoreDto
 import com.example.find_my_matzip.model.RestaurantDto
+import com.example.find_my_matzip.navTab.adapter.RankingRecyclerAdapter
 import com.example.find_my_matzip.navTab.adapter.RestaurantRecyclerAdapter
 import retrofit2.Call
 import retrofit2.Callback
@@ -19,6 +21,7 @@ import retrofit2.Response
 class RestaurantFragment : Fragment() {
     lateinit var binding: FragmentRestaurantBinding
     lateinit var adapter: RestaurantRecyclerAdapter
+    lateinit var adapter2: RankingRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentRestaurantBinding.inflate(layoutInflater)
@@ -30,12 +33,13 @@ class RestaurantFragment : Fragment() {
         binding = FragmentRestaurantBinding.inflate(layoutInflater, container, false)
 
         val restaurantService = (context?.applicationContext as MyApplication).restaurantService
-        val restaurantList = restaurantService.getResList()
+        val restaurantList = restaurantService.getAllRestaurantsByAvgScore()
+        val avgScoreList = restaurantService.getAllRestaurantsByAvgScore()
 
-        restaurantList.enqueue(object : Callback<List<RestaurantDto>> {
+        restaurantList.enqueue(object : Callback<List<ResWithScoreDto>> {
             override fun onResponse(
-                call: Call<List<RestaurantDto>>,
-                response: Response<List<RestaurantDto>>
+                call: Call<List<ResWithScoreDto>>,
+                response: Response<List<ResWithScoreDto>>
             ) {
                 val restaurantList = response.body()
                 if (restaurantList != null && restaurantList.isNotEmpty()) {
@@ -43,13 +47,14 @@ class RestaurantFragment : Fragment() {
                     val layoutManager = LinearLayoutManager(requireContext())
                     binding.resListRecyclerView.layoutManager = layoutManager
                     adapter = RestaurantRecyclerAdapter(this@RestaurantFragment,restaurantList)
+                    Log.d("sdotest", "${firstRestaurant.res_thumbnail}")
 //                    binding.rankingRecyclerView.adapter = RankingRecyclerAdapter(requireContext(), restaurantList)
                     binding.resListRecyclerView.adapter = adapter
                 } else {
 
                 }
             }
-            override fun onFailure(call: Call<List<RestaurantDto>>, t: Throwable) {
+            override fun onFailure(call: Call<List<ResWithScoreDto>>, t: Throwable) {
                 t.printStackTrace()
                 call.cancel()
             }
