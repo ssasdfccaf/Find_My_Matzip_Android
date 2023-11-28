94dd96fbce39e7f6660305de75f84cda994be032
옥 <eastok95@gmail.com>
2023-11-28 12:41:47 +0900

    지도 내 검색기능 1차완

--- app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/NearRestaurantFragment.kt
+++ app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/NearRestaurantFragment.kt
@@ -2,20 +2,19 @@ package com.example.find_my_matzip.navTab.navTabFragment
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.find_my_matzip.R
 import com.example.find_my_matzip.databinding.FragmentNearRestaurantBinding
 import com.example.find_my_matzip.model.ResWithScoreDto
 import com.example.find_my_matzip.navTab.adapter.NearRestaurantRecyclerAdapter
-import retrofit2.Call
-import retrofit2.Response
-import javax.security.auth.callback.Callback
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
-class NearRestaurantFragment : Fragment() {
+class NearRestaurantFragment : BottomSheetDialogFragment() {
     lateinit var binding: FragmentNearRestaurantBinding
     lateinit var adapter: NearRestaurantRecyclerAdapter
     var restaurantsInsideCircle: List<ResWithScoreDto> = emptyList() // 이 부분을 추가하고 초기화
@@ -36,6 +35,10 @@ class NearRestaurantFragment : Fragment() {
 
         val nearRestaurantList = restaurantsInsideCircle
 
+        // TextView에 식당 갯수 설정
+        binding.bottomSheetTitleTextView.text = "범위 내 식당 (${nearRestaurantList.size}개)"
+
+
         if (nearRestaurantList.isNotEmpty()) {
             val firstRestaurant = nearRestaurantList[0]
             val layoutManager = LinearLayoutManager(requireContext())
@@ -48,5 +51,36 @@ class NearRestaurantFragment : Fragment() {
         }
 
         return binding.root
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // BottomSheet의 레이아웃을 가져오기
+        val bottomSheet = dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+
+        // BottomSheetBehavior 가져오기
+        val bottomSheetBehavior = bottomSheet?.let { BottomSheetBehavior.from(it) }
+
+        // 최초에 보여지는 높이 설정
+        bottomSheetBehavior?.peekHeight = resources.getDimensionPixelSize(R.dimen.peek_height)
+
+        // BottomSheet 상태 변경 리스너 등록
+        bottomSheetBehavior?.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                // 상태 변경에 따른 동작 처리
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        // BottomSheet가 축소된 상태
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        // BottomSheet가 확장된 상태
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // 슬라이드 중일 때의 동작 처리
+            }
+        })
     }
 }
