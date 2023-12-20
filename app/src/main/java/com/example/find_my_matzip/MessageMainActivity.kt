cc67906131d221568c5c952b148ba78e8751f1dd
ssasdfccaf <sophierose062@gmail.com>
2023-12-21 01:38:51 +0900

    google-services.json = 기존 파이어베이스 구글 서비스

---
+++ app/src/main/java/com/example/find_my_matzip/MessageMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.find_my_matzip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.find_my_matzip.R
+
+class MessageMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_main)
+    }
+}
