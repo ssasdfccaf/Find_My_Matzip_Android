0b9c1b0c65263ed73b13e137a3fb574eb0225253
옥 <eastok95@gmail.com>
2024-01-09 10:59:22 +0900

    식당 삭제

--- app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/RestaurantDtlFragment.kt
+++ app/src/main/java/com/example/find_my_matzip/navTab/navTabFragment/RestaurantDtlFragment.kt
@@ -1,19 +1,30 @@
 package com.example.find_my_matzip.navTab.navTabFragment
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.example.find_my_matzip.AddRestaurantActivity
+import com.example.find_my_matzip.HomeTabActivity
+import com.example.find_my_matzip.LoginActivity
 import com.example.find_my_matzip.MyApplication
 import com.example.find_my_matzip.R
 import com.example.find_my_matzip.WriteReviewFragment
 import com.example.find_my_matzip.databinding.FragmentRestaurantDtlBinding
 import com.example.find_my_matzip.model.RestaurantDto
+import com.example.find_my_matzip.utiles.SharedPreferencesManager
+import com.example.find_my_matzip.utils.ConfirmDialog
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import es.dmoral.toasty.Toasty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,7 +32,8 @@ import retrofit2.Response
 
 class RestaurantDtlFragment : Fragment() {
     private var resId: Long = 0L
-    lateinit var binding : FragmentRestaurantDtlBinding
+    lateinit var binding: FragmentRestaurantDtlBinding
+    private var loginUserId: String = ""
 
     //resId 값으로 식당상세페이지 이동
     companion object {
@@ -44,25 +56,46 @@ class RestaurantDtlFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         resId = arguments?.getLong("resId") ?: 0L
-        binding = FragmentRestaurantDtlBinding.inflate(layoutInflater,container,false)
+        binding = FragmentRestaurantDtlBinding.inflate(layoutInflater, container, false)
+
+        loginUserId = SharedPreferencesManager.getString("id", "")
 
         val restaurantService = (context?.applicationContext as MyApplication).restaurantService
-        val restaurantDtl = arguments?.getLong("resId")?.let { restaurantService.getRestaurantDtl(it) }
+        val restaurantDtl =
+            arguments?.getLong("resId")?.let { restaurantService.getRestaurantDtl(it) }
 
-        Log.d("RestaurantDtlFragment","restaurantDtl.enqueue 호출 전 : $restaurantDtl")
+        Log.d("RestaurantDtlFragment", "restaurantDtl.enqueue 호출 전 : $restaurantDtl")
 
-        restaurantDtl?.enqueue(object : Callback<RestaurantDto>{
+        restaurantDtl?.enqueue(object : Callback<RestaurantDto> {
             override fun onResponse(call: Call<RestaurantDto>, response: Response<RestaurantDto>) {
 
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인.")
+                Log.d("RestaurantDtlFragment", "레스토랑 도착 확인.")
                 val restaurantDto = response.body()
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인1. : restaurantDto $restaurantDto")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인2. : restaurantDto.res_id ${restaurantDto?.res_id}")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인3. : restaurantDto.operate_time ${restaurantDto?.operate_time}")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인4. : restaurantDto.res_address ${restaurantDto?.res_address}")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인5. : restaurantDto.res_district ${restaurantDto?.res_district}")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인6. : restaurantDto.res_intro ${restaurantDto?.res_intro}")
-                Log.d("RestaurantDtlFragment","레스토랑 도착 확인7. : restaurantDto.avgScore ${restaurantDto?.avgScore}")
+                Log.d("RestaurantDtlFragment", "레스토랑 도착 확인1. : restaurantDto $restaurantDto")
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인2. : restaurantDto.res_id ${restaurantDto?.res_id}"
+                )
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인3. : restaurantDto.operate_time ${restaurantDto?.operate_time}"
+                )
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인4. : restaurantDto.res_address ${restaurantDto?.res_address}"
+                )
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인5. : restaurantDto.res_district ${restaurantDto?.res_district}"
+                )
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인6. : restaurantDto.res_intro ${restaurantDto?.res_intro}"
+                )
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "레스토랑 도착 확인7. : restaurantDto.avgScore ${restaurantDto?.avgScore}"
+                )
 
                 binding.resName.text = restaurantDto?.res_name.toString()
                 binding.resAddress.text = restaurantDto?.res_address.toString()
@@ -73,7 +106,8 @@ class RestaurantDtlFragment : Fragment() {
 
                 //소수 둘째자리에서 반올림
                 val ratingUpScore = restaurantDto?.avgScore?.toFloat()?.let {
-                    kotlin.math.round(it * 10) / 10.toFloat()}!!
+                    kotlin.math.round(it * 10) / 10.toFloat()
+                }!!
                 binding.resScoreText.text = ratingUpScore.toString()
                 binding.resScoreRating.rating = ratingUpScore
 
@@ -82,7 +116,10 @@ class RestaurantDtlFragment : Fragment() {
                     .override(900, 900)
                     .into(binding.resThumbnail)
 
-                Log.d("RestaurantDtlFragment", "도착 확인2: res_thumbnail ${restaurantDto?.res_thumbnail}")
+                Log.d(
+                    "RestaurantDtlFragment",
+                    "도착 확인2: res_thumbnail ${restaurantDto?.res_thumbnail}"
+                )
 
                 binding.toWriteReview.setOnClickListener {
                     Log.d("RestaurantDtlFragment", "게시글작성가기 클릭됨")
@@ -90,7 +127,7 @@ class RestaurantDtlFragment : Fragment() {
                     val resId = restaurantDto?.res_id
                     if (resId == 0L) {
                         Log.d("RestaurantDtlFragment", "resId is 비엇다~")
-                    }else{
+                    } else {
                         val fragment = WriteReviewFragment.newInstance(resId)
                         parentFragmentManager.beginTransaction()
                             .replace(com.example.find_my_matzip.R.id.fragmentContainer, fragment)
@@ -100,7 +137,7 @@ class RestaurantDtlFragment : Fragment() {
                 }
 
                 binding.mapBtn.setOnClickListener {
-                   // Toast.makeText(context,"지도뿅",Toast.LENGTH_SHORT).show()
+                    // Toast.makeText(context,"지도뿅",Toast.LENGTH_SHORT).show()
 
                     val res_lat = restaurantDto?.res_lat.toString()
                     val res_lng = restaurantDto?.res_lng.toString()
@@ -138,14 +175,99 @@ class RestaurantDtlFragment : Fragment() {
                     val tel = "tel:$callNum"
                     startActivity(Intent("android.intent.action.DIAL", Uri.parse(tel)))
                 }
-                
-                binding.searchBoard.setOnClickListener { 
+
+                binding.searchBoard.setOnClickListener {
                     //해당 식당 게시글 목록으로 이동
                     val fragment = HomeFragment.newInstance("", restaurantDto.res_id)
                     parentFragmentManager.beginTransaction()
                         .add(R.id.fragmentContainer, fragment)
                         .addToBackStack("HomeFragment")
                         .commit()
+                }
+
+                binding.editRestaurantButton.visibility =
+                    if (loginUserId == "admin") View.VISIBLE else View.INVISIBLE
+                binding.editRestaurantButton.setOnClickListener { }
+
+                fun sureDelete() {
+                    //로딩창 띄우기
+                    val storage = MyApplication.storage
+                    // 스토리지에 저장할 인스턴스
+                    val storageRef = storage.reference
+
+                    val imageUrl = response.body()!!.res_name+response.body()!!.res_address
+                    val imageName = deleteFirebaseImage(imageUrl)
+
+                    val imgRef = storageRef.child("restaurant_img/${imageName}")
+
+                    // 이미지 저장될 위치 및 파일명(파이어베이스)
+                    imgRef.delete().addOnCompleteListener {
+                        // 파일 삭제 성공
+                        Log.d("del res", " firestore 파일 삭제 성공")
+
+                    }.addOnFailureListener {
+                        // 파일 삭제 실패
+                        Log.d("del res", "firestore 파일 삭제 실패")
+                    }
+
+                    val call = restaurantService.deleteRestaurant((restaurantDto?.res_id!!))
+                    call.enqueue(object : Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            if (response.isSuccessful) {
+                                Log.d("del res", "식당 삭제 완료")
+                                Toasty.warning(
+                                    requireContext(),
+                                    "식당이 삭제 되었습니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val fragment = NewHomeFragment()
+                                parentFragmentManager.beginTransaction()
+                                    .replace(R.id.fragmentContainer, fragment)
+                                    .addToBackStack(null)
+                                    .commit()
+
+                            } else {
+                                Log.d("del res", "식당 삭제 실패")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            Log.d("del res", "통신 실패")
+                        }
+                    })
+                }
+
+                binding.deleteRestaurantButton.visibility =
+                    if (loginUserId == "admin") View.VISIBLE else View.INVISIBLE
+                binding.deleteRestaurantButton.setOnClickListener {
+                    val confirmDialog = ConfirmDialog(requireContext())
+
+                    val parentLayout =
+                        requireActivity().findViewById<ViewGroup>(android.R.id.content)
+                    val darkView = View(requireContext())
+                    darkView.layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    darkView.setBackgroundColor(Color.parseColor("#80000000")) // 어두운 배경색 지정 (이 경우는 50%의 검은색)
+
+                    confirmDialog.findViewById<TextView>(R.id.cancel_button).setOnClickListener {
+                        confirmDialog.dismiss()
+                        parentLayout.removeView(darkView)
+                    }
+
+                    confirmDialog.findViewById<TextView>(R.id.confirm_button).setOnClickListener {
+                        sureDelete()
+                        confirmDialog.dismiss()
+                        parentLayout.removeView(darkView)
+                    }
+                    // 다이얼로그 외부를 터치하여 닫을 때 어두운 배경 뷰 제거
+                    confirmDialog.setOnCancelListener {
+                        parentLayout.removeView(darkView)
+                    }
+                    parentLayout.addView(darkView)
+                    confirmDialog.show()
                 }
 
             }
@@ -155,11 +277,27 @@ class RestaurantDtlFragment : Fragment() {
             }
 
         })
-
-
-
-
         return binding.root
+    }
+
+    fun deleteFirebaseImage(imageName: String) {
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+
+
+            // 삭제할 이미지에 대한 참조 생성
+            val imageRef = storageRef.child("restaurant_img/$imageName.jpg")
+
+            // 이미지 삭제
+            imageRef.delete()
+                .addOnSuccessListener {
+                    // 이미지 삭제 성공
+                    Log.d("del res", "Firebase 이미지 삭제됨: $imageName")
+                }
+                .addOnFailureListener { e ->
+                    // 이미지 삭제 실패
+                    Log.e("del res", "Firebase 이미지 삭제 실패: $imageName", e)
+                }
 
     }
 }
